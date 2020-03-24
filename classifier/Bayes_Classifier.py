11b6b5bb63b724c6846a7f032ac066c1307fff1c
geneti <liuchang7032@outlook.com>
2020-03-24 05:09:14 -0400

    bayes debug

--- classifier/Bayes_Classifier.py
+++ classifier/Bayes_Classifier.py
@@ -13,28 +13,31 @@ P(words|class) --> likelihood
 P(class)	   --> prior
 '''
 class bayes_classifier(object):
-	def __init__(self, labels, eps = 0.01):
+	def __init__(self, labels, eps = 0.01, sigma = 0.00001):
 		# require labels format: numpy array
-		self.dimension = labels[len(labels)-1]
+		self.dimension = labels[len(labels)-1][0]
 		self.labels = labels
-		self.likelihood = dict.fromkeys(range(self.dimension))
-		for key in likelihood:
+		self.likelihood = dict.fromkeys(range(1,self.dimension+1))
+		for key in self.likelihood:
 			self.likelihood[key] = dict()
-		self.prior = dict.fromkeys(range(self.dimension), 0)
-		for s in labels:
-			self.prior[labels[s]] += 1
+		self.prior = dict.fromkeys(range(1,self.dimension+1), 0)
+		for s in self.labels:
+			self.prior[s[0]] += 1
 		self.evidence = dict()
 		self.eps = eps
+		self.sigma = sigma
 		self.vocab = set()
 		self.total_samples_count = len(labels)
 		self.total_words_count = 0
-		##self.count = dict.fromkeys(range(self.dimension), 0)
+		##self.count = dict.fromkeys(range(1,self.dimension+1), 0)
 
 	def train(self, data):
 		# require data format: numpy array
-		for iterator, sample in enumerate(data):
-			current_label = self.labels[iterator]
+		for iterator, s in enumerate(data):
+			current_label = self.labels[iterator][0]
 			##self.count[current_label] += len(sample)
+			sample = np.fromstring(s[0],  dtype = int, sep=',')
+			#print('sample:', sample)
 			self.total_words_count += len(sample)
 			for word in sample:
 				self.vocab.add(word)
@@ -46,28 +49,47 @@ class bayes_classifier(object):
 					self.likelihood[current_label][word] = 1
 				else:
 					self.likelihood[current_label][word] += 1
-			
+
 		# Normalize evidence, likelihood and prior
-		self.prior = self.prior/self.total_samples_count
-		self.evidence = self.evidence/self.total_words_count
-		for label_iterator in range(self.dimension):
-			class_sum = sum(evidence[label_iterator].values()) + len(evidence[label_iterator]) * self.eps
-			for key, dividend in self.evidence[label_iterator].items():
-				self.evidence[label_iterator][key] = (dividend + self.eps)/class_sum
+		for i in range(1,self.dimension+1):
+			self.prior[i] /= self.total_samples_count
+
+		for word in self.evidence.keys():
+			self.evidence[word] /= self.total_words_count
+
+		for label_iterator in range(1,self.dimension+1):
+			class_sum = sum(self.likelihood[label_iterator].values()) + len(self.likelihood[label_iterator]) * self.eps
+			for key, dividend in self.likelihood[label_iterator].items():
+				self.likelihood[label_iterator][key] = (dividend + self.eps)/class_sum
 
 	def test(self, data):
-		pred = np.array()
-		for iterator, sample in enumerate(data):
+		pred = np.array([])
+		for iterator, s in enumerate(data):
+			sample = np.fromstring(s[0], dtype = int, sep=',')
 			evidence = 1
+
+			average_evidence = sum(self.evidence.values())/self.total_words_count
 			for word in sample:
-				evidence *= self.evidence[word]
-			current_posterior = np.array()
-			for current_class in range(self.dimension):
+				if word not in self.evidence:
+					evidence *= average_evidence
+				else:
+					evidence *= self.evidence[word]
+			current_posterior = np.array([])
+			for current_class in range(1,self.dimension+1):
 				likelihood = 1
+
+				average_likelihood = sum(self.likelihood[current_class].values())/len(self.likelihood[current_class])
 				for word in sample:
-					likelihood *= self.likelihood[current_class][word]
+					if word not in self.likelihood[current_class]:
+						likelihood *= average_likelihood
+					else:
+						likelihood *= self.likelihood[current_class][word]
 				prior = self.prior[current_class]
-				current_posterior.append(likelihood*prior/evidence)
-			pred.append(max(current_posterior))
+				if likelihood==0 or evidence==0:
+					current_posterior = np.append(current_posterior, 0)
+				else:
+					current_posterior = np.append(current_posterior, likelihood*prior/evidence)
+			#print('max:', current_posterior)
+			pred = np.append(pred, int(np.argmax(current_posterior)+1))
 		return pred
 
--- structured_data.py
+++ structured_data.py
@@ -1,14 +1,32 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import sys
+import pickle
+import json
+sys.path.insert(0, '/Users/liuchang/Desktop/msu_all/homework/data_mining/cse881/project/cse881-data-mining/')
+from classifier import Bayes_Classifier 
 
 # Load Original unstructured data
-train_label = pd.read_csv('/Users/liuchang/Desktop/msu_all/homework/data_mining/cse881/project/Training_Label.txt',\
-							 sep=',', header=None, engine='python')
-train_data = pd.read_csv('/Users/liuchang/Desktop/msu_all/homework/data_mining/cse881/project/Training.txt',\
-							 sep=' ', header=None, engine='python')
-test_data = pd.read_csv('/Users/liuchang/Desktop/msu_all/homework/data_mining/cse881/project/Test.txt',\
-							 sep=' ', header=None, engine='python')
+dataset_address = '/Users/liuchang/Desktop/msu_all/homework/data_mining/cse881/project/cse881-data-mining/dataset/'
+train_label = np.array(pd.read_csv(dataset_address + 'Training_Label.txt', sep=',', header=None, engine='python'))
+train_data = np.array(pd.read_csv(dataset_address + 'Training.txt', sep=' ', header=None, engine='python'))
+test_data = np.array(pd.read_csv(dataset_address + 'Test.txt', sep=' ', header=None, engine='python'))
 # Get Sample info
 N = len(train_data)
 
+classifier = Bayes_Classifier.bayes_classifier(train_label)
+classifier.train(train_data)
+print('---------Train Complete----------')
+print('evidence:', len(classifier.evidence))
+print('likelihood:', len(classifier.likelihood[1]))
+print('prior:', len(classifier.prior))
+# print('total_samples_count:', classifier.total_samples_count)
+# print('total_words_count:', classifier.total_words_count)
+# print('testdata:', classifier.likelihood[18])
+pred = classifier.test(test_data)
+print('pred:', pred)
+np.savetxt(dataset_address+'predict.txt', pred, fmt='%d')
+np.save(dataset_address+'likelihood.npy', classifier.likelihood)
+np.save(dataset_address+'prior.npy', classifier.prior)
+np.save(dataset_address+'evidence.npy', classifier.evidence)
